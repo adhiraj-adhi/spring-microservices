3c9672b8bcb0991b4cf82d257cec6b97b259f42b
adhiraj-adhi <learner70911@gmail.com>
2026-04-25 10:26:49 +0530

    Day-4: Add cards and loans microservices

---
+++ cards/src/main/java/com/microservices/cards/CardsApplication.java
@@ -0,0 +1,41 @@
+package com.microservices.cards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Cards microservice REST API Documentation",
+				description = "XYZ Cards microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Abhinav",
+						email = "adhi@example.com",
+						url = "Contact-URL"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "License-URL"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "XYZ Cards microservice REST API Documentation",
+				url = "External Doc URL"
+		)
+)
+public class CardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsApplication.class, args);
+	}
+
+}
---
+++ loans/src/main/java/com/microservices/loans/LoansApplication.java
@@ -0,0 +1,39 @@
+package com.microservices.loans;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Loans microservice REST API Documentation",
+				description = "XYZ Loans microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Abhinav",
+						email = "adhi@example.com",
+						url = "Contact-URL"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "URL-For-Licence"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "XYZ Loans microservice REST API Documentation",
+				url = "External-Doc-URL"
+		)
+)
+public class LoansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansApplication.class, args);
+	}
+}
