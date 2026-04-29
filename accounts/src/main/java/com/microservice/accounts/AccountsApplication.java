98187b689e7de5cedd01abd4ea815b091616bea7
adhiraj-adhi <learner70911@gmail.com>
2026-04-29 05:42:54 +0530

    Day 8: Add configserver and read from classpath

--- accounts/src/main/java/com/microservice/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/microservice/accounts/AccountsApplication.java
@@ -12,30 +12,30 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @OpenAPIDefinition(
-		info = @Info(     // main section users see first in Swagger UI.
-				title = "Accounts microservice REST API documentation",
-				description = "XYZBank's Accounts microservice REST API documentation",
-				version = "v1",
-				contact = @Contact(  // Who should someone reach out to?
-						name = "Abhinav",
-						email = "abhi@gmail.com",
-						url = "URL where user can reach out"
-				),
-				license = @License( // What are people allowed to do with this API (or its definition)?
-						name = "Apache 2.0",
-						url = "Link to full license text"
-				)
-		),
-		externalDocs = @ExternalDocumentation(
-				description = "Full Documentation of XYZBank's Accounts microservice REST API",
-				url = "This is a link to additional documentation like Confluence page, GitHub README, etc."
-		)
-		// Other details like extensions, security, servers, and tags can also be provided
+        info = @Info(     // main section users see first in Swagger UI.
+                title = "Accounts microservice REST API documentation",
+                description = "XYZBank's Accounts microservice REST API documentation",
+                version = "v1",
+                contact = @Contact(  // Who should someone reach out to?
+                        name = "Abhinav",
+                        email = "abhi@gmail.com",
+                        url = "URL where user can reach out"
+                ),
+                license = @License( // What are people allowed to do with this API (or its definition)?
+                        name = "Apache 2.0",
+                        url = "Link to full license text"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Full Documentation of XYZBank's Accounts microservice REST API",
+                url = "This is a link to additional documentation like Confluence page, GitHub README, etc."
+        )
+        // Other details like extensions, security, servers, and tags can also be provided
 )
 public class AccountsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AccountsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AccountsApplication.class, args);
+    }
 
 }
---
+++ accounts/src/main/java/com/microservice/accounts/controllers/TestController.java
@@ -0,0 +1,18 @@
+package com.microservice.accounts.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+    @Value("${accounts.message}")
+    private String accountsMessage;
+
+    @GetMapping("/test")
+    public String contactInfo() {
+        return accountsMessage;
+    }
+}
