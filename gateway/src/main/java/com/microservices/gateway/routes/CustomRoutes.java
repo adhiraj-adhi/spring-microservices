4398881ec52ecbefe60faffe215e1fe58d69452d
adhiraj-adhi <learner70911@gmail.com>
2026-05-04 13:01:37 +0530

    Day-12: Add Custom Routing

---
+++ gateway/src/main/java/com/microservices/gateway/routes/CustomRoutes.java
@@ -0,0 +1,29 @@
+package com.microservices.gateway.routes;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CustomRoutes {
+    @Bean
+    public RouteLocator routeLocatorBean(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("cards-service", p -> p
+                        .order(-1)
+                        .path("/xyzbank/cards/api/**")
+                        .filters(f -> f
+                                .stripPrefix(2)
+                                // .otherFilter()
+                        )
+                        .uri("lb://CARDS")
+                )
+                .route("loans-service", p -> p
+                        .order(-1)
+                        .path("/xyzbank/loans/api/**")
+                        .filters(f -> f.rewritePath("/xyzbank/loans/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://LOANS")
+                ).build();
+    }
+}
