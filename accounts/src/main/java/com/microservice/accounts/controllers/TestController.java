dc4027b5b96c897ac54963afc662dd110c08d97c
adhiraj-adhi <learner70911@gmail.com>
2026-05-10 17:24:34 +0530

    Day-19: Implement Rate Limiter on Accounts Microservice-Minor Changes

--- accounts/src/main/java/com/microservice/accounts/controllers/TestController.java
+++ accounts/src/main/java/com/microservice/accounts/controllers/TestController.java
@@ -1,6 +1,7 @@
 package com.microservice.accounts.controllers;
 
 import com.microservice.accounts.dtos.ConfigPropertiesDTO;
+import io.github.resilience4j.ratelimiter.RequestNotPermitted;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,11 +21,11 @@ public class TestController {
 
     @RateLimiter(name="buildInfo", fallbackMethod = "getBuildInfoFallback")
     @GetMapping("/build-info")
-    public String contactInfo() {
+    public String getBuildInfo() {
         return buildInfo;
     }
 
-    public String getBuildInfoFallback(Throwable throwable) {
+    public String getBuildInfoFallback(RequestNotPermitted requestNotPermitted) {
         return "Fallback Build version 1.0";
     }
 
